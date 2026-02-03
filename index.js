295b0124a4c5a828a0e7871c81dad6aa9c09aa86
Stéphane VAN CAENEGEM <stephane@vancaenegem.fr>
2026-02-03 13:43:18 +0100

    Modification chargement Electron

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
-const electron = require('electron');
 require ('./config/config');
 require ('./app/logger');
 require ('./app/app');
 
-
-
 __app.once ('ready', ()=>{  
     __logger.info ('app ready');
 
-    if (electron.BrowserWindow) {
-        if (__config.electron !== undefined && __config.electron !== null) {
+    if (__config.electron !== undefined && __config.electron !== null) {
+        const electron = require('electron');
+        if (electron.BrowserWindow) {
             let mainWindow = new electron.BrowserWindow(__config.electron.options);
             mainWindow.on('closed', ()=> {
                 mainWindow = null;
